9417affc5a958d20fcdd5d56c2bd72a983e10eb5
Muhammad Faisal Amir <faisalamircs@gmail.com>
2022-06-21 05:32:37 +0700

    Initial commit

---
+++ src/main/kotlin/com/frogobox/springkotlinnewcomer/SpringKotlinNewComerApplication.kt
@@ -0,0 +1,11 @@
+package com.frogobox.springkotlinnewcomer
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringKotlinNewComerApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringKotlinNewComerApplication>(*args)
+}
---
+++ src/test/kotlin/com/frogobox/springkotlinnewcomer/SpringKotlinNewComerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.frogobox.springkotlinnewcomer
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringKotlinNewComerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
